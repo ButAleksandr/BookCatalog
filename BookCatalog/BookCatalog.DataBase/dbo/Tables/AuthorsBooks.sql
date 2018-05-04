31b3f226a83b3243a5b05669030cee5622c4808a
Aleksandr But <but.aleksandr@apriorit.com>
2018-05-04 07:26:36 +0300

    #ISE-5391: Added views to getting books and authors list
    Added triggers for evaluation of author's books count field.

--- BookCatalog/BookCatalog.DataBase/dbo/Tables/AuthorsBooks.sql
+++ BookCatalog/BookCatalog.DataBase/dbo/Tables/AuthorsBooks.sql
@@ -1,7 +1,62 @@
-﻿CREATE TABLE [dbo].[AuthorsBooks] (
-    [AuthorId] INT NOT NULL,
-    [BookId]   INT NOT NULL,
-    CONSTRAINT [FK_AuthorsBooks_Author] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Author] ([Id]),
-    CONSTRAINT [FK_AuthorsBooks_Books] FOREIGN KEY ([BookId]) REFERENCES [dbo].[Books] ([Id])
+﻿CREATE TABLE [dbo].[AuthorsBooks]
+([AuthorId] INT NOT NULL,
+ [BookId]   INT NOT NULL,
+ CONSTRAINT [FK_AuthorsBooks_Author] FOREIGN KEY([AuthorId]) REFERENCES [dbo].[Author]([Id]),
+ CONSTRAINT [FK_AuthorsBooks_Books] FOREIGN KEY([BookId]) REFERENCES [dbo].[Books]([Id])
 );
+GO
 
+-- Trigger for increasing BooksCount field value after was added new book ~ author relation
+
+CREATE TRIGGER [dbo].[Trigger_AuthorsBooks_Add] ON [dbo].[AuthorsBooks]
+FOR INSERT
+AS
+     BEGIN
+         SET NOCOUNT ON;
+
+		 -- Get Author Id
+         DECLARE @authorId INT=
+(
+    SELECT [AuthorId]
+    FROM [inserted]
+);
+         
+		 -- Update book count for the author
+         UPDATE Author
+           SET
+               BookCount = BookCount + 1
+         WHERE Id = @authorId;
+     END;
+GO
+
+-- Trigger for decreasing BooksCount field value after was deleted a book ~ author relation
+
+CREATE TRIGGER [dbo].[Trigger_AuthorsBooks_Delete] ON [dbo].[AuthorsBooks]
+FOR DELETE
+AS
+     BEGIN
+         SET NOCOUNT ON;
+
+		 -- Get Author Id
+         DECLARE @authorId INT=
+(
+    SELECT [AuthorId]
+    FROM [deleted]
+);
+         
+		 -- Update book count for the author
+         UPDATE Author
+           SET
+               BookCount = BookCount - 1
+         WHERE Id = @authorId;
+     END;
+GO
+
+-- Trigger which disable actions of the table updating 
+CREATE TRIGGER [dbo].[Trigger_AuthorsBooks_Update] ON [dbo].[AuthorsBooks]
+INSTEAD OF UPDATE
+AS
+     BEGIN
+         RAISERROR('AuthorsBooks table does not support UPDATE action.', 16, 1);
+         ROLLBACK TRANSACTION;
+     END;
---
+++ BookCatalog/BookCatalog.DataBase/dbo/Views/BooksView.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW dbo.BooksView
+AS
+     SELECT b.Id AS BookId,
+            a.Id AS AuthorId,
+            a.FirstName,
+            a.LastName,
+            a.BookCount
+     FROM [dbo].[Books] AS b
+          INNER JOIN [dbo].[Author] AS a ON a.[Id] IN(SELECT [dbo].[AuthorsBooks].[AuthorId]
+                                                      FROM [dbo].[AuthorsBooks]);
