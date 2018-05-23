1c50fe17e49c2529ccd856a59d2d8b9541966e02
Aleksandr But <but.aleksandr@apriorit.com>
2018-05-23 17:08:18 +0300

    #ISE-5391: Added Unity DependencyResolver and BookCatalog.Bootstrap project.

---
+++ BookCatalog/BookCatalog.Bootstrap/Unity/DependencyResolver.cs
@@ -0,0 +1,22 @@
+﻿using Unity;
+
+namespace BookCatalog.Bootstrap.Unity
+{
+    class DependencyResolver
+    {
+        public static void RegisterTypes(IUnityContainer _container)
+        {
+            InitDataLayer(_container);
+        }
+
+        private static void InitDataLayer(IUnityContainer container)
+        {
+            container.RegisterType<Common.Data.IRepository, Data.Repository>();
+        }
+
+        private static void InitBusinessLayer(IUnityContainer container)
+        {
+            container.RegisterType<Common.Business.IBookDM, Business.DM.BookDM>();
+        }
+    }
+}
---
+++ BookCatalog/BookCatalog.Bootstrap/Unity/Setup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Unity;
+
+namespace BookCatalog.Bootstrap.Unity
+{
+    class Setup
+    {
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            IUnityContainer container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            DependencyResolver.RegisterTypes(container);
+        }
+
+        public static IUnityContainer GetUnityConfig()
+        {
+            return container.Value;
+        }
+    }
+}
