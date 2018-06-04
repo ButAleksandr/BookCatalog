1463d83a4d8ef0fc0a33b604ebf09824da9b506c
Aleksandr But <but.aleksandr@apriorit.com>
2018-06-04 15:57:08 +0300

    #ISE-5391: created book controller

--- BookCatalog/BookCatalog.Portal/App_Start/RouteConfig.cs
+++ BookCatalog/BookCatalog.Portal/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace BookCatalog.Portal
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Book", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ BookCatalog/BookCatalog.Portal/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using BookCatalog.Common.Business;
+using System.Web.Mvc;
+
+namespace BookCatalog.Portal.Controllers
+{
+    public class BookController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return ShowBooksList();
+        }
+
+        public ActionResult ShowBooksList()
+        {
+            var bookDM = Factory.GetService<IBookDM>();
+
+            return null;
+        }
+    }
+}
